91ae7d6a2d11572da91e828694229781f20ebb73
Mat4m0 <matthias.amon@me.com>
2024-01-23 07:43:54 +0100

    chore: update and small fixes

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/sidebar1/index">
+            to="/docs/main/Introduction">
             Get started 🚀
           </Link>
         </div>
